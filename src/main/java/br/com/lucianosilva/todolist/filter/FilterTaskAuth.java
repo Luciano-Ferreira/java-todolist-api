b095ee39e7c9d41a6d063e283144cfab97befb4d
lfs <luciano2023silva@gmail.com>
2023-11-03 01:31:26 -0300

    chore: fix readme, fix FilterTaskAuth

--- src/main/java/br/com/lucianosilva/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/lucianosilva/todolist/filter/FilterTaskAuth.java
@@ -38,7 +38,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     var servletPath = request.getServletPath();
 
     if (servletPath.startsWith("/tasks/")) {
-      System.out.println("passing in filter");
+      System.out.println("passing in filter");  
       var authorization = request.getHeader("Authorization");
 
       var authEncoded = authorization.substring("Basic".length()).trim();
